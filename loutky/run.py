b62182b720c52f7acf582207d5affef8efde96a3
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-05-02 12:08:53 +0200

    Puppets | Loutky

---
+++ loutky/run.py
@@ -0,0 +1,9 @@
+many = int(input("Enter how many [INT]\n"))
+maxweight = int(input("Enter max weight [INT]\n"))
+weights = []
+for x in range(many):
+    weights.append(int(input("Enter weight puppet [INT]\n")))
+
+total = sum(weights)
+result = round((total/20))
+print(f"You will need: {result}")
