ea7caffaad773c6d84ddeec343b86e13fe5d5b12
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-05-04 19:28:41 +0200

    Fully done

--- encrypt-decrypt/run.py
+++ encrypt-decrypt/run.py
@@ -1,69 +1,108 @@
-#Import the required Libraries
+#Imprtujeme potřebné moduly
 from tkinter import *
-from tkinter import ttk
 from tkinter import filedialog
+from cryptography.fernet import Fernet
 import os
-#Create an instance of Tkinter frame
+# Vytvoříme instanci Tkinteru
 win = Tk()
-#Set the geometry of the Tkinter frame
-win.geometry("450x250")
+# Nastavíme velikost okna a vlastnosti
+win.geometry("250x100")
 win.title('Encrypter')
 win.resizable(0,0)
 win.configure(background="gray")
 
-lbl_path = Label(win,text = "Path: \"\"",fg = "gray")
+## Funkce pro zobrazení popupu s informací o úspěšném dokončení
+def Succes(path = None):
+    global filepath # Nastavíme globální proměnnou filepath
+    succpopup = Toplevel(win) # Vytvoříme nové okno
+    succpopup.title("Succes") # Nastavíme titulek okna
+    succpopup.configure(background="gray") # Nastavíme barvu pozadí okna
+    if path != None:
+        lbl_succ = Label(succpopup, text=f"Succesfully encrypted file!\n\n Path to file: {filepath}.key",background="gray") # Vytvoříme label s textem při uspesném dokončení
+    else:
+        lbl_succ = Label(succpopup, text=f"Succesfully decrypted file!",background="gray") # Vytvoříme label s textem při uspesném dešifrování
+    btn_ok = Button(succpopup,text="OK",command=lambda: succpopup.destroy()) # Vytvoříme tlačítko pro zavření okna
 
-def prompt_pass_popup():
-    # Create a new window for the pop-up
-    popup = Toplevel(win)
-    popup.title("Enter passpharse")
-    popup.configure(background="gray")
+    # Umístíme widgety do okna
+    lbl_succ.pack(padx=10,pady=20)
+    btn_ok.pack(padx=10,pady=20)
+    btn_encrypt.configure(state="disabled")
+    btn_deencrypt.configure(state="disabled")
 
-    # Add some content to the pop-up
-    lbl_epass = Label(popup, text="Enter passpharse",background="gray")
-    fld_pass = Entry(popup,
-                        show="*")
-    lbl_repass = Label(popup, text="Re-Enter passpharse",background="gray")
-    fld_pass_confirm = Entry(popup,
-                        show="*")
-    btn_check = Button(popup,
-                       text="Check")
-    lbl_epass.pack()
-    fld_pass.pack(padx=10)
-    lbl_repass.pack()
-    fld_pass_confirm.pack()
-    btn_check.pack(padx=10,pady=20)
+    lbl_path.configure(text=f"Loaded file: <None!>") # Nastavíme text o nahraném souboru na <None!> protože už žádný není nahraný
+    filepath = None
 
-    # Wait for the pop-up to be closed before continuing
-    popup.wait_window()
+## Funkce pro šifrování souboru
+def encrypt():
+    global filepath # Nastavíme globální proměnnou filepath
+    btn_encrypt.configure(state="disabled") # Vypneme tlačítko pro šifrování a dešifrování
+    btn_deencrypt.configure(state="disabled")
 
+    rawkey = Fernet.generate_key() # Vygenerujeme klíč
+    with open(filepath+".key", 'wb') as mykey:
+        mykey.write(rawkey) # Uložíme klíč do souboru
+
+    f = Fernet(rawkey) # Vytvoříme instanci Fernetu
+
+    with open(filepath, 'rb') as org_file: # Otevřeme soubor který chceme šifrovat
+        original = org_file.read()
+        org_file.close()
+
+    encrypted = f.encrypt(original) # Zašifrujeme soubor
+
+    with open (filepath, 'wb') as encrypted_file: # Uložíme zašifrovaný soubor
+        encrypted_file.write(encrypted)
+        encrypted_file.close()
+        Succes(filepath) # Zavoláme funkci pro zobrazení popupu s informací o úspěšném dokončení
+        filepath = None # Nastavíme filepath na None
+
+## Funkce pro dešifrování souboru
+def decrypt():
+    btn_encrypt.configure(state="disabled") # Vypneme tlačítko pro šifrování
+    with open(filepath+".key", 'rb') as file: # Otevřeme soubor s klíčem
+        rawkey = file.read()
+        file.close()
+
+    f = Fernet(rawkey) # Vytvoříme instanci Fernetu
+
+    with open(filepath, 'rb') as org_file: # Otevřeme soubor který chceme dešifrovat
+        original = org_file.read()
+        org_file.close()
+
+    decrypted = f.decrypt(original) # Dešifrujeme soubor
+
+    with open(filepath, 'wb') as encrypted_file: # Uložíme dešifrovaný soubor
+        encrypted_file.write(decrypted)
+        encrypted_file.close()
+        os.remove(filepath+".key") # Smažeme soubor s klíčem
+        Succes() # Zavoláme funkci pro zobrazení popupu s informací o úspěšném dokončení
+
+## Funkce pro otevření dialogu pro výběr souboru
 def browseFiles():
-    filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File",)
+    global filepath
+    filepath = filedialog.askopenfilename(initialdir = "/",title = "Select a File")
       
     # Nastavíme text dialog na otevřený soubor
-    lbl_path.configure(text=f"File Opened: {os.path.dirname(filename)}\n{os.path.basename(filename)}")
+    if filepath == "":
+        lbl_path.configure(text=f"Loaded file: <None!>")
+        btn_encrypt.configure(state="disabled")
+        btn_deencrypt.configure(state="disabled")
+    else:
+        lbl_path.configure(text=f"Loaded file: {os.path.basename(filepath)}")
+        btn_encrypt.configure(state="normal")
+        btn_deencrypt.configure(state="normal")
 
-btn_getfile = Button(win,
-                        text = "Choose File",
-                        command = browseFiles)
-lbl_passinfo = Label(win,
-                        text="",
-                        pady=5,)
+# Vytvoříme potřebné widgety
+lbl_path = Label(win,text = "Loaded file: <None!>",fg = "black") #Text který ukazuje otevřený souábor
+btn_getfile = Button(win,text = "Choose File",command = browseFiles) # Tlačítko pro otevření dialogu pro výběr souboru
+btn_encrypt = Button(win,text = "Encrypt",command= encrypt,state="disabled") # Tlačítko pro šifrování
+btn_deencrypt = Button(win,text = "Decrypt",command= decrypt,state="disabled") # Tlačítko pro dešifrování
 
-btn_encrypt = Button(win,
-                        text = "Encrypt",
-                        command= prompt_pass_popup)
-btn_deencrypt = Button(win,
-                        text = "Decrypt")
+# Umístíme widgety do okna
+lbl_path.pack()  
+btn_getfile.pack(pady=10)
+btn_encrypt.pack(side="left",anchor="n",padx=10)
+btn_deencrypt.pack(side="right",anchor="n",padx=10)
 
-
-#label_file_explorer.grid(column = 1, row = 2,)
-#button_explore.grid(column = 1, row = 5,)
-#button_test.grid(column=0, row=6, columnspan=2, pady=10)
-lbl_path.pack()
-btn_getfile.pack()
-lbl_passinfo.pack()
-btn_encrypt.pack(side="left",anchor="n",padx=80)
-btn_deencrypt.pack(side="right",anchor="n",padx=80)
-
+# Spustíme hlavní smyčku
 win.mainloop()
