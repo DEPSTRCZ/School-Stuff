36423d8530d51f8065ff9e06113f769503d1cc2e
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-12-01 21:21:42 +0100

    OOP/Factory - Done

--- OOP/Factory/run.py
+++ OOP/Factory/run.py
@@ -27,7 +27,7 @@ class monitor(Product):
     def __str__(self):
         return f"""Monitor:
     Name: {self.name}
-    Resolution: {self.__resolution}
+    Resolution: {self.__resolution[0]}x{self.__resolution[1]}
     Diagonal Length: {self.__diagonal_length} inches
     """
 
@@ -42,29 +42,31 @@ class keyboard(Product):
     Technology: {self.__technology}
     """
 
-class mouse(Product):
+class hamster(Product): # Hamter / Hamster he identifies under different pronounces
     def __init__(self, **args):
         super().__init__(args.get("name"))
         self.__technology = args.get("technology")
     
     def __str__(self):
-        return f"""Mouse:
+        return f"""Hamter/Mouse/Hamter:
     Name: {self.name}
     Technology: {self.__technology}
     """
 
 class Factory:
-    product_type = ["pc","monitor","keyboard","mouse"]
+    product_type = ["pc","monitor","keyboard","hamster"]
     
     @staticmethod
     def produce(**args):
-        if args.get("type"):
+        if args.get("type").lower() in Factory.product_type:
             return globals()[args["type"].lower()](**args)
 
 products = []
 
-
 products.append(Factory.produce(type = "Pc",name = "Babičky PC",cpu = "Zinger Chicken",ram = 16))
-print(products)
+products.append(Factory.produce(type = "MoNiTor",name = "Azus",diagonal_length = 125,resolution = (1280,720)))
+products.append(Factory.produce(type = "keyBOARD",name = "LogiTek",technology = "wireless"))
+products.append(Factory.produce(type = "HamsteRs",name = "Pablo",technology = "cable"))
+
 for product in products:
     print(product)
