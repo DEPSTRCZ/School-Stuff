b1e0fab2cfe3b9c98b3e149589519b0169cf528f
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-12-01 21:04:37 +0100

    OOP/Factory

--- OOP/Factory/run.py
+++ OOP/Factory/run.py
@@ -5,18 +5,50 @@ class Product:
     def __str__(self):
         return f"{self.name}"
 
-class PC(Product):
-    def __init__(self, **kwargs):
-        print(kwargs["name"])
+class pc(Product):
+    def __init__(self, **args):
+        super().__init__(args.get("name"))
+        self.__cpu = args.get("cpu")
+        self.__ram = args.get("ram")
     
-class Monitor(Product):
+    def __str__(self):
+        return f"""Computer:
+    Name: {self.name}
+    Procesor: {self.__cpu}
+    Ram: {self.__ram} GB
+    """
+    
+class monitor(Product):
+    def __init__(self, **args):
+        super().__init__(args.get("name"))
+        self.__resolution = args.get("resolution")
+        self.__diagonal_length = args.get("diagonal_length")
+    
+    def __str__(self):
+        return f"""Monitor:
+    Name: {self.name}
+    Resolution: {self.__resolution}
+    Diagonal Length: {self.__diagonal_length} inches
+    """
+
+class keyboard(Product):
     pass
 
-class Keyboard(Product):
-    pass
-
-class Mouse(Product):
+class mouse(Product):
     pass
 
 class Factory:
-    pass
+    product_type = ["PC","monitor","keyboard"]
+    
+    @staticmethod
+    def produce(**args):
+        if args.get("type"):
+            return globals()[args["type"].lower()](**args)
+
+products = []
+
+
+products.append(Factory.produce(type = "Pc",name = "Babičky PC",cpu = "Zinger Chicken",ram = 16))
+print(products)
+for product in products:
+    print(product)
